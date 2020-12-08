a9faad01a57b98c8e8b0c75d4be43ed846688e6b
Tom <tom@example.com>
2020-12-08 17:11:07 +0100

    fkn kutpuzzel werkt niet

--- Day8/aoc8.py
+++ Day8/aoc8.py
@@ -4,28 +4,53 @@ with open(file) as f:
 	data = f.readlines()
 
 programs = [line.rstrip() for line in data]
-operations = [program.split() for program in programs]
 
-i = 0
-accumulator = 0
-operated = set()
+def run(programs):
+	i = 0
+	accumulator = 0
+	operated = set()
+	operations = [program.split() for program in programs]
+	while i < len(programs):
+		operation, operator = operations[i]
+		operator = int(operator)
 
-while i < len(programs):
-	operation, operator = operations[i]
-	operator = int(operator)
+		if i in operated:
+			return accumulator
+			break
 
-	if i in operated:
-		break
+		operated.add(i)
 
-	operated.add(i)
+		if operation == "jmp":
+			i += operator
 
-	if operation == "jmp":
-		i += operator
+		elif operation == "nop":
+			i += 1
 
-	elif operation == "nop":
-		i += 1
-	elif operation == "acc":
-		accumulator += operator
-		i += 1
+		elif operation == "acc":
+			accumulator += operator
+			i += 1
+	
 
-print(accumulator)
+answer1 = run(programs)
+print(answer1)
+
+programlist = []
+
+for x in range(len(programs)):
+	for program in programs:
+		progs = []
+		programcopy = programs[:]
+
+		if "jmp" in programcopy[x]:
+			program.replace("jmp", "nop", 1)
+
+		elif "nop" in programcopy[x]:
+			program.replace("nop", "jmp", 1)
+
+		progs.append(programcopy)
+	programlist.append(progs)
+
+for program in programlist:
+	if run(program) != 1930:
+		print(run(program))
+	
